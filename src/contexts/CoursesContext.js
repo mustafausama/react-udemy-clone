a5d0884196e587ac8612f9c778a90a2d049a2012
mustafausama <mustafausama@outlook.com>
2022-08-27 04:14:00 +0200

    edit: Incorrect URL used for error demonstration

--- src/contexts/CoursesContext.js
+++ src/contexts/CoursesContext.js
@@ -3,7 +3,7 @@ import useFetch from "../hooks/useFetch";
 
 export const CoursesContext = createContext();
 
-const URL = "https://api.jsonbin.io/v3/b/63096e31e13e6063dc8dafff?meta=false";
+const URL = "https://api.jsonbin.io/v3/b/incorrect";
 const CoursesContextProvider = ({ children }) => {
   const { data: courses, loading, error } = useFetch(URL);
   return (
